b17839b11825004398a839c9b18249be3f1888e6
Lukaz Rocha <lukazrocha05@gmail.com>
2022-03-09 16:24:40 -0300

    adiciona pasta do exercício 02 - uso do bind

---
+++ exercicios/ex02/index.js
@@ -0,0 +1,24 @@
+// Crie um objeto de nome Ave e adicione um atributo de nome espécie.
+
+// Dentro do objeto Ave crie uma função, que retorno o atributo espécie.
+
+// Retorne o objeto invocando a função criada.
+
+// Após o retorno do objeto invocando a função, 
+//crie um objeto de nome Peixe e através do bind faça ele retornar a espécie do objeto peixe.
+
+const ave = {
+    especie: 'Pavão',
+    getEspecie: function(){
+        return this.especie;
+    }
+}
+let especieAve = ave.getEspecie();
+console.log(especieAve);
+
+const peixe = {
+    especie: 'Tucunaré'
+}
+
+let especiePeixe = ave.getEspecie.bind(peixe);
+console.log(especiePeixe());
